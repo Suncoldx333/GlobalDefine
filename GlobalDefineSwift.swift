a7196a2a4e007ca751efd851dcca1b0625045049
WangZhaoyun <1073138529@qq.com>
2017-05-02 17:15:10 +0800

    tableviewcell_height
    tableview Extension about cell height cache

--- GlobalDefineSwift.swift
+++ GlobalDefineSwift.swift
@@ -10,6 +10,7 @@ import UIKit
 
 let ScreenWidth : CGFloat = UIScreen .main .bounds .size .width
 let ScreenHeight : CGFloat = UIScreen .main .bounds .size .height
+typealias cellConfigurationBlock = (UITableViewCell) ->Void
 
 
 //颜色，Eg:ColorMethodho(0x00c18b)
@@ -135,6 +136,72 @@ func makeSheetTitleDic(title : String,titleFont : String,titleColor : String) ->
     titleDic.setObject(titleFont, forKey: "font" as NSCopying)
     titleDic.setObject(titleColor, forKey: "textcolor" as NSCopying)
     return titleDic
+}
+
+extension UITableView{
+    
+    var heightCache : cellHeightCache? {
+        set{
+            objc_setAssociatedObject(self, cellHeightKey.cellHeightCacheKey, heightCache, objc_AssociationPolicy.OBJC_ASSOCIATION_COPY_NONATOMIC)
+        }
+        get{
+            return objc_getAssociatedObject(self, cellHeightKey.cellHeightCacheKey) as? cellHeightCache
+        }
+    }
+    
+    
+    func cellHeightCacheWith(identifier : String?,cacheKey : String?,configuration : cellConfigurationBlock) -> CGFloat {
+        
+        var height : CGFloat = 0
+        
+        if identifier == nil || cacheKey == nil {
+            return height
+        }
+        
+        height = cellHeightWith(identifier: identifier, configuration: configuration)
+        
+        return height
+    }
+    
+    private func cellHeightWith(identifier : String?,configuration : cellConfigurationBlock) -> CGFloat {
+        var height : CGFloat = 0
+        
+        if identifier == nil {
+            return height
+        }
+        
+        let preLayoutCell : UITableViewCell = cellForReuse(identifier: identifier)
+        preLayoutCell.prepareForReuse()
+        configuration(preLayoutCell)
+        
+        height = fittingHeightFor(cell: preLayoutCell)
+        return height
+    }
+    
+    private func cellForReuse(identifier : String?) -> UITableViewCell{
+        var preLayoutCellByIdentifiers : [String : UITableViewCell]? = objc_getAssociatedObject(self, objcKey) as? [String : UITableViewCell]
+        if preLayoutCellByIdentifiers == nil {
+            preLayoutCellByIdentifiers = [:]
+            objc_setAssociatedObject(self, objcKey, preLayoutCellByIdentifiers, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+        
+        var preLayoutCell : UITableViewCell? = preLayoutCellByIdentifiers?[identifier!]
+        if preLayoutCell == nil {
+            preLayoutCell = self.dequeueReusableCell(withIdentifier: identifier!)
+            preLayoutCell?.contentView.translatesAutoresizingMaskIntoConstraints = false
+            preLayoutCellByIdentifiers?.updateValue(preLayoutCell!, forKey: identifier!)
+        }
+        
+        return preLayoutCell!
+    }
+    
+    private func fittingHeightFor(cell : UITableViewCell) -> CGFloat {
+        var height : CGFloat = 0
+        
+        height = cell.contentView.frame.size.height
+        
+        return height
+    }
 }
 
 class GlobalDefineSwift: NSObject {
